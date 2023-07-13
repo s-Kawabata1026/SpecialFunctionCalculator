4bb3c06a0264c48957bc5b2dc1f8f7dc97ab3bdb
s-Kawabata1026 <138741067+s-Kawabata1026@users.noreply.github.com>
2023-07-13 11:16:39 +0900

    Delete answer.h

--- answer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//‚±‚±‚Í\‘¢‘Ì‚ÌéŒ¾
-
-struct ans {	//\‘¢‘Ì‚ÌéŒ¾
-	int ans_selection;	//‘I‘ğˆ‚Ì•Û‘¶
-	int ans_int;		//intŒ^‚Ì“š‚¦‚Ì•Û‘¶
-	double ans_double;	//doubleŒ^‚Ì“š‚¦‚Ì•Û‘¶
-};
